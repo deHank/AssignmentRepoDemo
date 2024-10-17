83e2cc87b0844944995a7dd5464e9973fd2ee7e8
NICHOLAS AUGUSTUS <nicholasaugustus1@gmail.com>
2024-10-17 14:38:52 -0400

    Added room number variable to Patient Type class

--- src/AddElements.java
+++ src/AddElements.java
@@ -3,9 +3,9 @@ public class AddElements {
     public void execute() {
         boolean on = true;
         LinkedList<PatientType> patientList = new LinkedList<PatientType>();
-        PatientType patient1 = new PatientType("John", "Smith", "123-45-6789", "23132312",
+        PatientType patient1 = new PatientType("John", "Smith", "123-45-6789", "23132312", "123",
                 1, 2, 2001, "Harry", true, "This is a chart", "Yay youre a patient");
-        PatientType patient2 = new PatientType("Jane", "Smith", "321-54-9876", "80085",
+        PatientType patient2 = new PatientType("Jane", "Smith", "321-54-9876", "80085", "124",
                 11, 12, 2005, "Harry", true, "This is also a chart", "Dang... youre a patient");
         do {
             patientList.add(patient1);
