f871f37ae64346fdcba0e6ce0ba80091934a1c2b
FlavoredReed <jacobalexander587@gmail.com>
2024-10-28 12:32:13 -0400

    Updated window.java
    update

--- Sprint 1/UI/Parts/Window.java
+++ Sprint 1/UI/Parts/Window.java
@@ -1,18 +1,35 @@
 package UI.Parts;
 
-import javax.swing.JFrame;
 import java.awt.Component;
+import javax.swing.JFrame;
 
 public class Window extends JFrame{
     final String DEFAULTTITLE = "";
     final int DEFAULTSIZE = 500;
     final int DEFAULTLOCATION = 300;
+
+    public Window (String title){
+        super(title);
+        super.setSize(DEFAULTSIZE, DEFAULTSIZE);
+        super.setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(true);
+    }
+
+    public Window (String title, int width, int height){
+        super(title);
+        super.setSize(width, height);
+        super.setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(true);
+    }
     
     public Window(String title, int x, int y, int width, int height){
         super(title);
-        super.setSize(DEFAULTSIZE, DEFAULTSIZE);
-        super.setLocation(DEFAULTLOCATION, DEFAULTLOCATION);
+        super.setSize(width, height);
+        super.setLocation(x, y);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(true);
     }
 
     public void addItem(String borderLayout, Component comp){
--- Sprint 1/UI/Windows/HomePage.java
+++ Sprint 1/UI/Windows/HomePage.java
@@ -1,5 +1,10 @@
 package UI.Windows;
 
+import UI.Parts.*;
+
 public class HomePage {
-    
+    public static void main(String[] args) {
+        Window HomeWindow = new Window("Home", 500, 500, 500, 500);
+        HomeWindow.setVisible(true);
+    }
 }
