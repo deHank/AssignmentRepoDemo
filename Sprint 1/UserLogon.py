01f3215394f42c3e47b83b06ba29955f2aea00c0
Spencer Devine <devinesg@vcu.edu>
2024-10-30 15:05:21 -0400

    Skeleton code

---
+++ Sprint 1/UserLogon.py
@@ -0,0 +1,25 @@
+@dataclass
+class UserInfo:
+   firstName: str
+   email: str
+   password: str
+   
+class UserLogin
+
+   def __init__(self) -> None:
+      self.UserArray = [] # holds UserInfo objects
+   
+   def login():
+      pass
+
+      
+   def main():
+      while():
+         print("1: login, 2: create account, 3: quit")
+         userIn = input("Select Option")
+         if(userIn == "1"):
+            pass
+         elif(userIn == "2"):
+            pass
+         elif(userIn == "3"):
+            break
